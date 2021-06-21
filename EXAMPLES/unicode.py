dbff6bfe7d4bbe2a6008916bb843502675dc573d
jstrickler <jstrickler@gmail.com>
2021-06-21 13:03:48 -0400

    added unicode

--- EXAMPLES/unicode.py
+++ EXAMPLES/unicode.py
@@ -1,7 +1,13 @@
-#!/usr/bin/python3
-print('we spent \u20ac1.23M for an original C\u00e9zanne')
-print('26\u00B0')
-print('26\N{DEGREE SIGN}')
-print('26\u00B0')
-print(r'26\u00B0\n')
-print(r'26\N{DEGREE SIGN}')
+#!/usr/bin/env python
+
+print('26\u00B0')  # <1>
+print('26\N{DEGREE SIGN}')  # <2>
+print(r'26\u00B0\n')  # <3>
+print()
+
+print('we spent \u20ac1.23M for an original C\u00e9zanne') # <4>
+print("Romance in F\u266F Major")
+print()
+
+data = ['\U0001F95A', '\U0001F414']  # <5>
+print(sorted(data))
